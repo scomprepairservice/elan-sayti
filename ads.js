89c4cfd4e2dcd4e424b0085305b6fcdeeb623f0f
scomprepairservice <scomprepairservice@gmail.com>
2026-09-16 19:58:13 +0400

    Add files via upload

--- ads.js
+++ ads.js
@@ -2,7 +2,7 @@
 const ads=[
  {id:1,title:'Dell Inspiron noutbuk',price:'450 AZN',category:'Noutbuk',location:'Sumqayıt',image:'https://placehold.co/800x500?text=Dell+Inspiron',description:'Ətraflı məlumat üçün əlaqə saxlayın.'},
  {id:2,title:'İkinci elan nümunəsi',price:'100 AZN',category:'Elektronika',location:'Bakı',image:'https://placehold.co/800x500?text=Elan',description:'Bu nümunə elandır. ads.js faylından dəyişdirə bilərsiniz.'}
- {id:1,title:'Dell Inspiron noutbuk',price:'450 AZN',category:'Noutbuk',location:'Sumqayıt',image:'https://placehold.co/800x500?text=Dell+Inspiron',description:'Ətraflı məlumat üçün əlaqə saxlayın.'},
- {id:2,title:'İkinci elan nümunəsi',price:'100 AZN',category:'Elektronika',location:'Bakı',image:'https://placehold.co/800x500?text=Elan',description:'Bu nümunə elandır. ads.js faylından dəyişdirə bilərsiniz.'}
+ {id:3,title:'Lenovo',price:'450 AZN',category:'Noutbuk',location:'Sumqayıt',image:'https://placehold.co/800x500?text=Dell+Inspiron',description:'Ətraflı məlumat üçün əlaqə saxlayın.'},
+ {id:4,title:'Acer',price:'100 AZN',category:'Elektronika',location:'Bakı',image:'https://placehold.co/800x500?text=Elan',description:'Bu nümunə elandır. ads.js faylından dəyişdirə bilərsiniz.'}
 
 ];
